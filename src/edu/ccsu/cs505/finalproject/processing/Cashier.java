6be80e25356c2edae5bc00be10d60b295cd94328
Kelly <martink4@scsu.edu>
2020-10-27 18:57:35 -0400

    making cashier interact with customer, ordering strategy

--- src/edu/ccsu/cs505/finalproject/processing/Cashier.java
+++ src/edu/ccsu/cs505/finalproject/processing/Cashier.java
@@ -8,22 +8,43 @@ public class Cashier {
 
     String foodPick;
     List<String> toppingsPick = new ArrayList<String>();
+
     public void processOrder() {
         System.out.println("Which number would you like to order?");
         Scanner scanner = new Scanner(System.in);
+
         int pick = scanner.nextInt();
-        String topping = null;
-        if(pick==1){
-            foodPick="Pizza";
-            System.out.println("Which topping would you like on your pizza? Enter one at a time.Type end to finish");
-            while(topping != "end"){
+//        String topping = scanner.hasNext()
+        boolean keepGoing = true;
+        if (pick == 1) {
+            foodPick = "Pizza";
+            System.out.println("Which topping would you like on your pizza? Enter one at a time.Type q to finish");
+//            boolean user = true;
+            String topping = scanner.nextLine();
+            while (keepGoing) {
                 toppingsPick.add(topping);
+                topping = scanner.nextLine();
+                if (topping.equals("q")) {
+                    keepGoing = false;
+                }
             }
-            foodPick="Grinder";
-            System.out.println("Which topping would you like on your grinder? Enter one at a time. Type end to finish");
-            while(topping != "end"){
+
+            }else{
+            foodPick = "Grinder";
+            System.out.println("Which topping would you like on your grinder? Enter one at a time.Type q to finish");
+            String topping = scanner.nextLine();
+            while (keepGoing) {
                 toppingsPick.add(topping);
+                topping = scanner.nextLine();
+                if (topping.equals("q")) {
+                    keepGoing = false;
+                }
+
+
             }
         }
+        System.out.println("So you'll have a " + foodPick + " with " + toppingsPick);
+//        send to cashregister
+//        send to cook
     }
 }
