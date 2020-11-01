604bbbf35e98d6b08c4ce99a0f7532d63b29321f
Maciej Beclawski <m.beclawski@my.ccsu.edu>
2020-11-01 14:21:02 -0500

    added javadocs to Food class

--- src/edu/ccsu/cs505/finalproject/food/Food.java
+++ src/edu/ccsu/cs505/finalproject/food/Food.java
@@ -4,30 +4,49 @@ import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
 
+/**
+ * Defines abstract Food with toppings
+ */
 public abstract class Food {
 
 	private List<String> toppings;
 
+	/**
+	 * constructor
+	 */
 	public Food()
 	{
 		this.toppings = new ArrayList<String>();
 	}
 
+	/**
+	 * Template method for food preparation with hook methods
+	 * @throws InterruptedException
+	 */
 	public void prepare() throws InterruptedException {
 		placeToppings();
 		cook();
 		slice();
 	}
 
+	/**
+	 * @param topping list of food toppings as Strings
+	 */
 	public void addTopping(String topping){
 		this.toppings.add( topping );
 	}
 
+	/**
+	 * @return list of toppings
+	 */
 	public List<String> getToppings()
 	{
 		return this.toppings;
 	}
 
+	/**
+	 * @return string representation of child food classes
+	 */
 	public String toString()
 	{
 		StringBuilder result = new StringBuilder(this.getClass().getSimpleName());
@@ -52,6 +71,10 @@ public abstract class Food {
 		return result.toString();
 	}
 
+	/**
+	 * Prints each topping as it is being prepared
+	 * @throws InterruptedException
+	 */
 	private void placeToppings() throws InterruptedException {
 		for ( String toppping : toppings) {
 			System.out.println(("Adding " + toppping));
@@ -60,7 +83,15 @@ public abstract class Food {
 		}
 	}
 
+	/**
+	 * Template Patterns hook method used in prepare() method
+	 * @throws InterruptedException
+	 */
 	abstract void cook() throws InterruptedException;
 
+	/**
+	 * Template Patterns hook method used in prepare() method
+	 * @throws InterruptedException
+	 */
 	abstract void slice() throws InterruptedException;
 }
