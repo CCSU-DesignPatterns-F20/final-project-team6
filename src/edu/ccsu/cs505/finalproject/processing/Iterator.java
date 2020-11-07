ac5cd166b6a9f392290336532b06d7fca96d77d7
Kelly <martink4@scsu.edu>
2020-11-07 12:46:43 -0500

    fixing up iterator and menu classes

--- src/edu/ccsu/cs505/finalproject/processing/Iterator.java
+++ src/edu/ccsu/cs505/finalproject/processing/Iterator.java
@@ -1,7 +1,7 @@
 package edu.ccsu.cs505.finalproject.processing;
 
-public interface Iterator {
-    public Object next();
+public interface Iterator<T> {
+    public T next();
     public Boolean hasNext();
     public void remove();
 }
--- src/edu/ccsu/cs505/finalproject/processing/Menu.java
+++ src/edu/ccsu/cs505/finalproject/processing/Menu.java
@@ -5,13 +5,13 @@ import java.util.*;
 /**
  * Prints current menu for the customer to choose from
  */
-public class Menu<String> {
+public class Menu<T> {
 
         public Iterator iterator;
 
         //menu will be in array
-        ArrayList<String> menu = new ArrayList<String>();
-        menu.add("Burger");
+        List<T> menu = new ArrayList<T>();
+//        menu.add("Pizza");
 
 
     private class MenuIterator implements Iterator {
@@ -19,23 +19,31 @@ public class Menu<String> {
         int index;
 
         @Override
-        public boolean hasNext() {
+        public Boolean hasNext() {
 
-            if(index < menu.length){
+            if(index < menu.size()){
                 return true;
             }
             return false;
         }
 
         @Override
-        public Object next() {
+        public void remove() {
+
+        }
+
+
+
+        @Override
+        public T next() {
 
             if(this.hasNext()){
-                return menu[index++];
+                return menu.get(index++);
             }
             return null;
         }
     }
+
     public static void showMenu(){
         System.out.println("1. Pizza \n toppings available- pepperoni, mushrooms, sausage, peppers.");
         System.out.println("2. Grinder \n toppings available- lettuce, onions, ham, turkey, cheese, cucumbers.");
